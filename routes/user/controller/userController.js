b767a7c069bf658539c6429f22ad01f0a2b6bf5a
Alondradaisy <alondra.warren96@gmail.com>
2021-08-18 10:05:44 -0700

    build err helper page to alert user if something went wrong while browsing

--- routes/user/controller/userController.js
+++ routes/user/controller/userController.js
@@ -2,3 +2,10 @@
 const bcrypt = require("bcryptjs");
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
+
+async function createUser(req, res) {
+  try {
+  } catch (e) {
+    res.status(500).json();
+  }
+}
