d4a1891599a098e43eca571e7efbc65be9a3615d
Alondradaisy <alondra.warren96@gmail.com>
2021-08-18 09:42:58 -0700

    install and require jwt, passport, and bcrypt for user authentication

--- routes/user/controller/userController.js
+++ routes/user/controller/userController.js
@@ -1,1 +1,4 @@
 // async functions to log user entry
+const bcrypt = require("bcryptjs");
+const User = require("../model/user");
+const jwt = require("jsonwebtoken");
--- routes/user/model/user.js
+++ routes/user/model/user.js
@@ -1,1 +1,26 @@
 //mongoose schema for user
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
