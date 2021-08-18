750ecbebc5c390427b1d7871fc7f5ccd176a8ac2
Alondradaisy <alondra.warren96@gmail.com>
2021-08-18 12:43:52 -0700

    add post reqs to allow user to create a profile and login in userRouter

---
+++ routes/user/userRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const { createUser, login } = require("./controller/userController");
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/create-user", createUser);
+router.post("/login", login);
+
+module.exports = router;
