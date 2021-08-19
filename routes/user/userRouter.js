2f54c23df2f1de1077254c9f0ce506af8512680a
Alondradaisy <alondra.warren96@gmail.com>
2021-08-19 11:08:51 -0700

    bring in axios to make API call with stripe

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
 const passport = require("passport");
 const { createUser, login } = require("./controller/userController");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
+router.get("/", async (req, res) => {
+  let data = await axios.get(
+    "https://"
+    {
+      headers: {
+        Accept: "application/json",
+        "Authorization: Bearer sk_test_4eC39HqLyjWDarjtT1zdp7dc"
+      },
+    }
+  );
+
   res.send("respond with a resource");
 });
 
--- routes/utils/passportAuth.js
+++ routes/utils/passportAuth.js
@@ -1,1 +1,31 @@
-//authentication for entry
+//authentication for entry using passport
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const keys = process.env.JWT_USER_SECRET_KEY;
+
+const jwtOps = {};
+
+jwtOps.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOps.secretOrKey = keys;
+
+const useJWTLoginStrategy = new JwtStrategy(jwtOps, async (payload, done) => {
+  const userEmail = payload.userEmail;
+
+  try {
+    if (userEmail) {
+      const user = await User.findOne({ email: userEmail }).select("-password");
+
+      if (!user) {
+        return done(null, false);
+      } else {
+        return done(null, user);
+      }
+    } else {
+      return done(null, false);
+    }
+  } catch (e) {
+    return done(e, false);
+  }
+});
