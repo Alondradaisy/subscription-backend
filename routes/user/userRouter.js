1c61cd447a7134e2b05075307f5e31fd0bc57c9a
Alondradaisy <alondra.warren96@gmail.com>
2021-08-20 17:31:30 -0700

    incorporate router requests to user, create user, login path, path to update profile and logout paths

--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,25 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const axios = require("axios");
 const passport = require("passport");
 const { createUser, login } = require("./controller/userController");
 
 /* GET users listing. */
-router.get("/", async (req, res) => {
-  let data = await axios.get(
-    "https://"
-    {
-      headers: {
-        Accept: "application/json",
-        "Authorization: Bearer sk_test_4eC39HqLyjWDarjtT1zdp7dc"
-      },
-    }
-  );
-
+router.get("/", async (req, res, next) => {
   res.send("respond with a resource");
 });
 
 router.post("/create-user", createUser);
 router.post("/login", login);
+
+router.put("./update-profile", passport.authenticate("jwt-user"), {
+  session: false,
+}),
+  function (req, res) {
+    res.send("Success!");
+  };
+
+router.get("/logout", function (req, res) {
+  res.clearCookie("jwt-cookie");
+  res.send("Logged out!");
+});
 
 module.exports = router;
